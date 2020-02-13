8a4ff7e5fd494143209ec4630320da02273973d6
HISSANDANISH <59311045+HISSANDANISH@users.noreply.github.com>
2020-02-13 22:02:10 +0530

    After updation
    updated the filter method to where,to reduce the work of ruby and let database do filtering of records for us.

--- L4-ActiveRecord/todo.rb
+++ L4-ActiveRecord/todo.rb
@@ -1,14 +1,20 @@
-require 'active_record'
+require "active_record"
 
 class Todo < ActiveRecord::Base
-  def overdue?
-    due_date < Date.today
+  def self.overdue
+    all.where("due_date < ?", Date.today)
   end
+
+  def self.due_today
+    all.where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    all.where("due_date > ?", Date.today)
+  end
+
   def due_today?
     due_date == Date.today
-  end
-  def due_later?
-    due_date > Date.today
   end
 
   def to_displayable_string
@@ -16,24 +22,25 @@ class Todo < ActiveRecord::Base
     display_date = due_today? ? nil : due_date
     "#{id}. #{display_status} #{todo_text} #{display_date}"
   end
+
   def self.show_list
     puts "My todolist\n"
     puts "Overdue\n"
-    puts all.filter{|todo| todo.overdue?}.map{|todo| todo.to_displayable_string}
+    puts overdue.map { |todo| todo.to_displayable_string }
     puts "\n\n"
     puts "Due Today\n"
-    puts all.filter{|todo| todo.due_today?}.map{|todo| todo.to_displayable_string}
+    puts due_today.map { |todo| todo.to_displayable_string }
     puts "\n\n"
     puts "Due Later\n"
-    puts all.filter{|todo| todo.due_later?}.map{|todo| todo.to_displayable_string}
+    puts due_later.map { |todo| todo.to_displayable_string }
     puts "\n\n"
-    end
+  end
   def self.add_task(todo)
-    create!(todo_text: todo[:todo_text],due_date: Date.today+todo[:due_in_days])
+    create!(todo_text: todo[:todo_text], due_date: Date.today + todo[:due_in_days], completed: false)
   end
   def self.mark_as_complete!(todo_id)
-    todo=Todo.find(todo_id)
-    todo.completed=true
+    todo = Todo.find(todo_id)
+    todo.completed = true
     todo.save
     todo
   end
